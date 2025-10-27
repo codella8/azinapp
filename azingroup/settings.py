d7945a346a3a246ece3bea079bc8782271ba51c1
shammohmmdi399@gmail.com <codella.dev02@gmail.com>
2025-10-27 04:15:06 -0700

    database setup

--- azingroup/settings.py
+++ azingroup/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'azingroup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'azindb',
+        'USER': 'postgres', 
+        'PASSWORD': 'sham1384',  
+        'HOST': 'localhost',  
+        'PORT': '5432',  
     }
 }
 
