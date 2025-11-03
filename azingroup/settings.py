748c11ad2bae98c5ef25fd47d72311803629fd0a
shammohmmdi399@gmail.com <codella.dev02@gmail.com>
2025-11-03 01:23:10 -0800

    edited

--- azingroup/settings.py
+++ azingroup/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--qy@2yn56u^$rx)sq0s0b3$-&g=%4@sz)1%mdfj1=%4)0*c_0l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['azingroup.com', 'www.azingroup.com', '145.223.97.51']
+DEBUG = True
+ALLOWED_HOSTS = []
 
 # Application definition
 
